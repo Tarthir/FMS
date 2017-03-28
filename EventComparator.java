a73c60929dc170373251a5686ad98d71fa82b5d0
Tarthir <tylerbrady34@gmail.com>
2017-03-27 18:08:31 -0600

    Add files via upload

---
+++ EventComparator.java
@@ -0,0 +1,60 @@
+package models;
+
+import java.util.Comparator;
+
+/**
+ * Created by tyler on 3/27/2017.
+ * A comparator class for Events
+ */
+
+public class EventComparator implements Comparator<Event> {
+    public EventComparator() {
+    }
+
+    @Override
+    public int compare(Event event, Event event2) {
+        int comp = compareBirthDeath(event,event2);
+        if(comp != 0){
+            return comp;
+        }
+
+        comp = compareYearAndSpelling(event,event2);
+        return comp;
+    }
+    /**Checking two events, and returning whether one happened earlier than the other. If no dates are provided
+     * then we order alpabetically
+     * */
+    private static int compareYearAndSpelling(Event event, Event event2) {
+        if(event.getYearInt() == 0 && event2.getYearInt() != 0){return 1;}//if there isnt a year in one
+        if(event.getYearInt() != 0 && event2.getYearInt() == 0){return -1;}
+        if(event.getYearInt() < event2.getYearInt()){
+            return -1;
+        }
+        else if(event.getYearInt() > event2.getYearInt()){
+            return 1;
+        }
+        else{//might return 1,-1, or zero
+            return event.getEventType().compareTo(event2.getEventType());
+
+        }
+    }
+
+    /**
+     * Compares two events, checking if either is a birth or death event and returning accordingly
+     * */
+    private static int compareBirthDeath(Event event, Event event2){
+        if(event.getEventType().toLowerCase().equals("birth") && !event2.getEventType().toLowerCase().equals("birth") ){
+            return -1;
+        }
+        else if(!event.getEventType().toLowerCase().equals("birth") && event2.getEventType().toLowerCase().equals("birth")){
+            return 1;
+        }
+        if(event.getEventType().toLowerCase().equals("death")){
+            return 1;
+        }
+        else if(event2.getEventType().toLowerCase().equals("death")){
+            return -1;
+        }
+        return 0;
+    }
+}
--- MapsFrag.java
+++ MapsFrag.java
@@ -25,28 +25,46 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.tylerbrady34gmail.familyclient.Models.Model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
+import java.util.TreeSet;
 
 import models.Event;
+import models.EventComparator;
+import models.Person;
 
 
 /**
  * A map fragment class
  */
 public class MapsFrag extends Fragment implements OnMapReadyCallback, OnMarkerClickListener {
-    /**Our google map*/
+    /**
+     * Our google map
+     */
     private GoogleMap mMap;
-    /**Our tetview*/
+    /**
+     * Our tetview
+     */
     private TextView currTextView;
-    /**Our map allowing us to easily access events from map markers*/
-    private Map<String,Event> eventMap = new TreeMap<>();
+    /**
+     * Our map allowing us to easily access events from map markers
+     */
+    private Map<String, Event> eventMap = new TreeMap<>();
+    /**
+     * A list of our polylines on the screen
+     */
+    List<Polyline> polylines = new ArrayList<>();
 
     public MapsFrag() {
         // Required empty public constructor
@@ -82,70 +100,162 @@ public class MapsFrag extends Fragment implements OnMapReadyCallback, OnMarkerCl
         mMap.setOnMarkerClickListener(this);
         if (Model.getFilter().isShowFathersSide()) {
             addMapMarker(Model.getPaternalAncestors());
-           // doLines(Model.getPaternalAncestors());
         }
         if (Model.getFilter().isShowMothersSide()) {
             addMapMarker(Model.getMaternalAncestors());
-           // doLines(Model.getMaternalAncestors());
         }
     }
 
-  /*  private void doLines(Set<String> sideOfAncestors) {
-        for(String id : sideOfAncestors){
-
-        }
-        if(Model.getSettings().isShowFamilyLines()){
-
-        }
-        if(Model.getSettings().isShowLifeLines()){
-
-        }
-        if(Model.getSettings().isShowSpouseLines()){
-
-        }
-
-        PolylineOptions line=
-                new PolylineOptions().add(new LatLng())
-                        .width(5).color(Color.RED);
-
-        mMap.addPolyline(line);
-    }*/
 
     /**
      * Puts in all the map markers on the GoogleMap
+     *
      * @param people all the people being added to the map
-     * */
+     */
     private void addMapMarker(Set<String> people) {
         //for every person, grab their lat and long of each of their events and add it to the map
         for (String person : people) {
             List<Event> eventList = Model.getPersnEvntMap().get(person);
-            for(int i = 0; i < eventList.size(); i++) {//for each event
+            for (int i = 0; i < eventList.size(); i++) {//for each event
                 Event event = eventList.get(i);
                 double latitude = Double.parseDouble(event.getLatitude());
                 double longitude = Double.parseDouble(event.getLongitude());
                 String city = event.getCity();
                 MarkerOptions marker = new MarkerOptions().position(new LatLng(latitude, longitude)).title(city);
-                eventMap.put(marker.getTitle(),event);
+                eventMap.put(marker.getTitle(), event);
                 mMap.addMarker(marker);
             }
         }
     }
 
 
-
     @Override
     public boolean onMarkerClick(Marker marker) {
-        Log.d("MapFrag","Entering onMarkerClick");
+        Log.d("MapFrag", "Entering onMarkerClick");
         Event event = eventMap.get(marker.getTitle());
         String fName = Model.getPeople().get(event.getPersonID()).getfName();
         String lName = Model.getPeople().get(event.getPersonID()).getlName();
-        currTextView.setText(fName +" " + lName + "\n" + event.toString() );
-
+        currTextView.setText(fName + " " + lName + "\n" + event.toString());
+        doLines(event.getPersonID(), marker);
 
         // Return false to indicate that we have not consumed the event and that we wish
         // for the default behavior to occur (which is for the camera to move such that the
         // marker is centered and for the marker's info window to open, if it has one).
         return false;
+    }
+
+    private void doLines(String personClicked, Marker marker) {
+        clearLines();
+        if (Model.getSettings().isShowFamilyLines()) {
+            doFamilyLines(personClicked);
+        }
+        if (Model.getSettings().isShowLifeLines()) {
+            doLifeLines(personClicked);
+        }
+        if (Model.getSettings().isShowSpouseLines()) {
+            doSpouseLines(personClicked, marker);
+        }
+    }
+    /**Draws the lines from the clicked to his parents to their parents and so on
+     * @param personClicked , the person who was clicked
+     * */
+    private void doFamilyLines(String personClicked) {
+        String father = Model.getPeople().get(personClicked).getFather();
+        linesHelperFather(father);
+        String mother = Model.getPeople().get(personClicked).getMother();
+        linesHelperMother(mother);
+    }
+    /**A recursive function to draw family lines
+     * @param mother , The mother of the current generation
+     * */
+    private void linesHelperMother(String mother) {
+    }
+
+    /**A recursive function to draw family lines
+     * @param father , The father of the current generation
+     * */
+    private void linesHelperFather(String father) {
+        String nextGenFather = Model.getPeople().get(father).getFather();
+        /*Collections.sort(Model.getPersnEvntMap().get(father), new EventComparator());
+        Event currBirth =
+        LatLng pos = new LatLng(Double.parseDouble(event.getLatitude()), Double.parseDouble(event.getLongitude()));
+        LatLng pos2 = new LatLng(Double.parseDouble(event2.getLatitude()), Double.parseDouble(event2.getLongitude()));*/
+    }
+
+    /**
+     * Holds the logic for drawing life lines. Meaning the events of a persons life
+     *
+     * @param personClicked, the personID of the event clicked
+     */
+    private void doLifeLines(String personClicked) {
+        List<Event> events = Model.getPersnEvntMap().get(personClicked);
+        //EventComparator compare = new EventComparator();
+        //Collections.sort(events, compare);//TODO could just do this in the Model class
+        for (int i = 0; i < events.size() - 1; i++) {//go through all the events chronologically
+            Event event = events.get(i);
+            Event event2 = events.get(i + 1);
+            LatLng pos = new LatLng(Double.parseDouble(event.getLatitude()), Double.parseDouble(event.getLongitude()));
+            LatLng pos2 = new LatLng(Double.parseDouble(event2.getLatitude()), Double.parseDouble(event2.getLongitude()));
+            drawLines(pos, pos2);
+        }
+    }
+
+    /**
+     * Holds all the logic for doing Spouse lines, meaning a line i drawn from the clicked on to the spouses birth palce
+     *
+     * @param personClicked, the personID of the event clicked
+     * @param marker         The marker object that was clicked on
+     */
+    private void doSpouseLines(String personClicked, Marker marker) {
+        String spouse = Model.getPeople().get(personClicked).getSpouse();
+        if (spouse.equals("")) {
+            return;
+        }//if there is no spouse
+        List<Event> events = Model.getPersnEvntMap().get(spouse);
+        Event firstEvent = null;
+
+        if (events.size() > 0) {
+            firstEvent = events.get(0);
+        }//grab the first event
+
+        for (int i = 0; i < events.size(); i++) {//will only draw lines if the spouse has events
+            Event event = events.get(i);
+            if (Integer.parseInt(event.getYear()) < Integer.parseInt(firstEvent.getYear())) {//update the earliest event
+                firstEvent = event;
+            }
+            if (event.getEventType().toLowerCase().equals("birth")) {//
+                LatLng pos = new LatLng(Double.parseDouble(event.getLatitude()), Double.parseDouble(event.getLongitude()));
+                drawLines(pos, marker.getPosition());
+            } else if (i == events.size() - 1) {
+                LatLng pos = new LatLng(Double.parseDouble(firstEvent.getLatitude()), Double.parseDouble(firstEvent.getLongitude()));
+                drawLines(pos, marker.getPosition());
+            }
+        }
+    }
+
+    /**
+     * Clears the lines from the map when we are done with them
+     */
+
+    private void clearLines() {
+        if (polylines.size() > 0) {
+            for (Polyline line : polylines) {
+                line.remove();
+            }
+        }
+    }
+
+    /**
+     * Draws the lines on the map connecting people
+     *
+     * @param pos, a Latlng Position
+     * @param pos2 a Latlng position
+     */
+    private void drawLines(LatLng pos, LatLng pos2) {
+        PolylineOptions line = new PolylineOptions().add(pos, pos2).width(6).color(Color.MAGENTA);
+        polylines.add(mMap.addPolyline(line));
+
+
     }
 
 
